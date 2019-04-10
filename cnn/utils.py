59f65d7b8cd0c2274df443047db794a17d0e6648
sudarshan <sdevkota007@gmail.com>
2019-04-10 19:37:04 +0000

    dataset name modified

--- cnn/utils.py
+++ cnn/utils.py
@@ -1,13 +1,13 @@
 import os
 
-def remove_non_existing_datafiles(dataset):
-    for dataset_name in dataset:
+def remove_non_existing_datafiles(data_sets):
+    for dataset_name in data_sets:
         full_filename = '../data/cnn.csail.mit.edu/motif_occupancy/' + dataset_name
         if os.path.isdir(full_filename):
             pass
         else:
             print("Warning: Directory {} not found.".format(full_filename))
-            dataset.remove(dataset_name)
+            data_sets.remove(dataset_name)
 
-    return dataset
+    return data_sets
 
